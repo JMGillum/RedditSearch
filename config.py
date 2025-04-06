4a062681ffee96d68d8f38ffed211ec0840adc6f
JMGillum2 <bearoshdragonson@gmail.com>
2025-04-06 09:33:40 -0700

    Added comment stating required section

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+# -------- REQUIRED --------
 client_id="your_client_id" # your client id
 client_secret="your_client_secret" # your client secret
 user_agent="Python:better-search:v1.0.0 (by u/your_username)" # your user agent
+# -------- -------- --------
 
 searches_file="searches.json" # Default is searches.json. Change this value to use a different file
 link_output="links.txt" # This is where urls are saved when you request to save them
