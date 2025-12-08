f924adb130b4277c699976b4b31bd6fb7cf60768
JMGillum <gillum0927@gmail.com>
2025-12-07 18:47:45 -0800

    removed redundant refresh

--- scroll.py
+++ scroll.py
@@ -121,7 +121,6 @@ class ScrollingList:
                 self.screen.addstr(ticker, 0, f"{item}")
                 ticker = ticker + 1
                 if numLines is not None and ticker >= numLines:
-                    self.screen.refresh()
                     break
             self.screen.refresh()
 
